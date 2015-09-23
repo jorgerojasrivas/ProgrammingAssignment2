0b29eca30d59adceba0163b10ff92cc94e239029
JorgeRojasRivas <jorge.rojasrivas@ucr.ac.cr>
2015-09-22 19:21:51 -0600

    adding code to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,24 @@
 ## cacheSolve does the actual computation of the inverse of the object returned
 ## by makeCacheMatrix. cacheSolve uses the solve(X)function in R. 
 
-## Write a short comment describing this function
+## If the inverse has already been calculated, and the matrix has not changed,
+## then cacheSolve gets the inverse from cache memory. 
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y){
+    
+    x <<- y
+    m <<- NULL
+    
+  }
+  
+  get <- function() x
+  set_inverse <- function(iinverse) m <<- inverse
+  get_inverse <- function () m
+  
+  list(set = set, get = get, set_inverse )
 
 }
 
